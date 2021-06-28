de93889f57ac73a19eeb5361989ed37dda743be1
김태형 <thk12378905@gmail.com>
2021-06-28 10:39:53 +0000

    컨테이너 순서

---
+++ docker-node-mysql/node/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# 아래 "db_url" 따옴표유지
+mysql_host="db_url"
+mysql_port=3306
+
+# netcat으로 3306이 있는지 확인
+# nc db_url 3306
+
+# mysql container가 뜨기전엔 시작안함
+while ! nc $mysql_host $mysql_port; do  
+  >&2 echo "Mysql is unavailable"
+  sleep 1
+done
+
+# Nodejs 시작
+echo "Start nodejs"  
+node  /src/app.js
